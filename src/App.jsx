7bc280fc7958b35fbeb97ac2b868bd806ee5e4f4
Swapnil Tomake <swapnil@inspiremindstech.com>
2023-01-08 17:34:42 +0530

    added storibook.js.org library

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Portfolio from './pages/LandingPage/Portfolio/Portfolio';
+
+function App() {
+  return (
+    <Portfolio></Portfolio>
+  );
+}
+
+export default App;
